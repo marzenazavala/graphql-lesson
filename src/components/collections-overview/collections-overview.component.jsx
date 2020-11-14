72a2e0a7197ea72d930bf641e187b921b7cafff6
Marzena Bortnowska-Zavala <marzenazavala@gmail.com>
2020-11-14 19:58:49 +0100

    graphql queries

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
 import CollectionPreview from '../collection-preview/collection-preview.component';
-
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
-
 import './collections-overview.styles.scss';
 
 const CollectionsOverview = ({ collections }) => (
@@ -16,8 +10,5 @@ const CollectionsOverview = ({ collections }) => (
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
-});
 
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
